5c67400cd0b8d21abd4f3c468c45c4d03971cb17
reza107-hub <rashidaminur104@gmail.com>
2023-05-30 19:57:57 +0600

    initilazing server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello from bistro boss restaurant')
+})
+
+app.listen(port)
